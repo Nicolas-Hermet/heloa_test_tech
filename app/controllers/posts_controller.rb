5cb1d236efd5e416f137bbb87d0d1d06335d5e89
Nicolas Hermet <nicolas.hermet@lisaia.com>
2025-04-23 18:58:54 +0200

    Add show view just for fun

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,8 @@ class PostsController < ApplicationController
     @query = params[:query].presence
     @pagy, @posts = @query ? pagy(Post.search_by_content_title_and_tags(@query), items: 10) : pagy(Post.all, items: 10)
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
 end
