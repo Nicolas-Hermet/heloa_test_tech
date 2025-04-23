e3e1b9f56982bff0b9b8ca5af7fcc16d3500506b
Nicolas Hermet <nicolas.hermet@lisaia.com>
2025-04-23 14:06:54 +0200

    Clean associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
   before_create :assign_uuid
 
   enum visibility: { public: 'public', internal: 'internal' }, _prefix: :visibility
+  has_many :posts_tags, dependent: :destroy
+  has_many :tags, through: :posts_tags, dependent: :destroy
+  has_one :primary_post_tag, class_name: 'PrimaryPostsTag', dependent: :destroy
+  has_one :primary_tag, through: :primary_post_tag, source: :tag
 
   private
 
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,5 @@
 class Tag < ApplicationRecord
+  has_many :posts_tags, dependent: :destroy
+  has_many :posts, through: :posts_tags, dependent: :destroy
+  has_one :primary_posts_tag, dependent: :destroy
 end
